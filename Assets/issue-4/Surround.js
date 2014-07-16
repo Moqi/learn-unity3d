3c15327746f5363a8d6b911c46f62fd043739b1a
G_will <g_will@ieqi.com>
2014-07-16 15:45:52 +0800

    增加场景；修改运动环绕方式；issue #4

--- Assets/issue-4/Surround.js
+++ Assets/issue-4/Surround.js
@@ -1,13 +1,64 @@
 ﻿#pragma strict
 
+var cube1:GameObject;
+var cube2:GameObject;
+var center:Vector3;
+var isSurrondCube1:boolean = true;
+var surroundPoint:Vector3;
+var s:float = 0;
+var speed:float = 3.0;
+
 function Start () {
 	Debug.Log("Start ...");
+	
+	cube1 = GameObject.Find("Cube1");
+
+	Debug.Log("Cube1 position ===> ");
+	Debug.Log(cube1.transform.position);
+	
+	cube2 = GameObject.Find("Cube2");
+	Debug.Log("Cube2 position ===> ");
+	Debug.Log(cube2.transform.position);
+	
+	center = Vector3(
+		(cube1.transform.position.x+cube2.transform.position.x)/2,
+		(cube1.transform.position.y+cube2.transform.position.y)/2,
+		(cube1.transform.position.z+cube2.transform.position.z)/2
+	);
+	Debug.Log("center position ===> ");
+	Debug.Log(center);
+	
+	transform.position = center;
+	
+	surroundPoint = cube1.transform.position;
+}
+
+
+function changeSurroundPoint() {
+	 isSurrondCube1 = !isSurrondCube1;
+}
+
+function getSurroundPoint() {
+	if (isSurrondCube1) {
+		return cube1.transform.position;
+	} else {
+		return cube2.transform.position;
+	}
+}
+
+function getSurroundDirection() {
+	if (isSurrondCube1) {
+		return Vector3.up;
+	} else {
+		return Vector3.down;
+	}
 }
 
 function Update () {
-	var x = Mathf.Sin(Time.time*2)*5;
-	var z = Mathf.Sin(Time.time)*20;
-    transform.position.x = x;
-    transform.position.z = z;
-    Debug.Log(transform.position);
+	if (s>360){
+		changeSurroundPoint();
+		s=0;
+	}
+	transform.RotateAround(getSurroundPoint(), getSurroundDirection(), speed);
+	s += speed;
 }
