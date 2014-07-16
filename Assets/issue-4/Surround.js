c7ed14a6c855cb16dd6782edc0b0407317b34975
G_will <g_will@ieqi.com>
2014-07-16 12:36:35 +0800

    物体运动 issue #4

---
+++ Assets/issue-4/Surround.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+	Debug.Log("Start ...");
+}
+
+function Update () {
+	var x = Mathf.Sin(Time.time*2)*5;
+	var z = Mathf.Sin(Time.time)*20;
+    transform.position.x = x;
+    transform.position.z = z;
+    Debug.Log(transform.position);
+}
