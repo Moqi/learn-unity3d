4a531eec8916c0da7692a766aa508bd90a4b89cb
G_will <g_will@ieqi.com>
2014-07-17 14:51:34 +0800

    增加 ScriptableObject 测试； issue #7

---
+++ Assets/ScriptableObjectTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : ScriptableObject {
+
+	public int attack = 20;
+	public int magic = 30;
+	public int maxHP = 100;
+	public int maxMp = 100;
+	public Texture2D icon = (Texture2D)Resources.Load ("avatar");
+
+}
+
+public class ScriptableObjectTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Hero hero = ScriptableObject.CreateInstance<Hero> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
